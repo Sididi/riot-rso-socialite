547a45ea65726e05a6a7be007c22a6a821d0af68
Sid <my_sididi@hotmail.fr>
2023-12-20 01:38:03 +0100

    Now also calling /lol/summoner/v4/summoners/me route to retrieve more informations about LoL summoner

--- Provider.php
+++ Provider.php
@@ -63,7 +63,13 @@ class Provider extends AbstractProvider
                 ],
             ]);
 
-            return json_decode($response->getBody(), true) + ['cpid' => $cpid];
+            $response2 = $this->getHttpClient()->get('https://' . $cpid . '.api.riotgames.com/lol/summoner/v4/summoners/me', [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]);
+
+            return json_decode($response->getBody(), true) + json_decode($response2->getBody(), true) + ['cpid' => $cpid];
         }
 
         return null;
@@ -75,18 +81,17 @@ class Provider extends AbstractProvider
      */
     protected function mapUserToObject(array $user)
     {
-        /* Commented fields should now be fetched from the /lol/summoner/v4/summoners/me endpoint through an additional call since Riot summoner names changes */
         return (new User())->setRaw($user)->map([
-            'id'       => $user['puuid'], //$user['id'],
+            'id'       => $user['id'],
             'nickname' => $user['gameName'] . '#' . $user['tagLine'],
             'name'     => $user['gameName'] . '#' . $user['tagLine'],
             'email'    => null,
-            'avatar'   => null, //$user['profileIconId'],
+            'avatar'   => $user['profileIconId'],
 
-            //'accountId' => $user['accountId'],
+            'accountId' => $user['accountId'],
             'puuid' => $user['puuid'],
-            //'revisionDate' => $user['revisionDate'],
-            //'summonerLevel' => $user['summonerLevel'],
+            'revisionDate' => $user['revisionDate'],
+            'summonerLevel' => $user['summonerLevel'],
             'cpid' => $user['cpid'],
         ]);
     }
